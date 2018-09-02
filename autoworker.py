60124cde555c9bb75f797524af83dd4325b1e9f6
shinjo <sktometometo@gmail.com>
2018-09-02 23:34:07 +0900

    add prompt

--- autoworker.py
+++ autoworker.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
 from selenium import webdriver
@@ -7,6 +7,7 @@ import os
 import sys
 import time
 import csv
+import random
 from email.mime.text import MIMEText
 from email.header import Header
 from email.utils import formatdate
@@ -129,18 +130,27 @@ def schedule():
         list_schedule = readSchedule( filepath_schedule )
 
         today = datetime.datetime.now()
-        nextmonthday = today + datetime.timedelta(31)
+        nextmonthday = today + datetime.timedelta(30)
         list_syussya, list_taisya = createSchedulesInMonth( today.year, today.month, list_schedule )
         list_syussya_nextmonth, list_taisya_nextmonth = createSchedulesInMonth( nextmonthday.year, nextmonthday.month, list_schedule )
         list_syussya.extend( list_syussya_nextmonth )
         list_taisya.extend(  list_taisya_nextmonth )
         debugprint( "INFO", "schedule() has got schedules" )
         
-        schedule_obj  = sched.scheduler( time.time, time.sleep )
+        #schedule_obj  = sched.scheduler( time.time, time.sleep )
+
+        time_margin_insec = 5
+        time_margin_range_min =  1 * 60 / time_margin_insec #  1 min
+        time_margin_range_max = 20 * 60 / time_margin_insec # 20 min
 
         for i, ( time_syussya, time_taisya ) in enumerate( zip( list_syussya, list_taisya ) ):
-            schedule_obj.enterabs( time.mktime(time_syussya.timetuple()), 2*i + 1, autoworker, ("go",) )
-            schedule_obj.enterabs( time.mktime(time_taisya.timetuple()),  2*i + 2, autoworker, ("taisya",) )
+            time_syussya = time.mktime(time_syussya.timetuple()) - time_margin_insec*random.randint(time_margin_range_min,time_margin_range_max)
+            time_taisya  = time.mktime(time_taisya.timetuple())  + time_margin_insec*random.randint(time_margin_range_min,time_margin_range_max)
+            handler_syussya = schedule_obj.enterabs( time_syussya, 2*i + 1, autoworker, ("go",) )
+            handler_taisya  = schedule_obj.enterabs( time_taisya,  2*i + 2, autoworker, ("taisya",) )
+            datetime_syussya = datetime.datetime.fromtimestamp( time_syussya )
+            datetime_taisya  = datetime.datetime.fromtimestamp( time_taisya )
+            list_schedhandler.append( [[datetime_syussya,handler_syussya],[datetime_taisya,handler_taisya]] )
         
         if not is_debug:
             debugprint( "INFO", "schedule() run" )
@@ -185,15 +195,35 @@ def main():
     thread_obj = threading.Thread( target=schedule, daemon=True )
     thread_obj.start()
     while True:
-        stop = input("Input \"stop\" and press enter to stop program. > ")
-        if stop == "stop":
+        print(" \"stop\" and press enter to stop program. ")
+        print(" \"list\" and press enter to list schedules. ")
+        print(" \"cancel\" and press enter to cancel a schedule. ")
+        inputline = input("Input > ")
+        if inputline == "stop":
             debugprint( "INFO", "main(), stop of program" )
             sys.exit()
+        elif inputline == "list": 
+            print("schedules:")
+            for i, schedhandler in enumerate(list_schedhandler):
+                print( ( "   " + str(i) )[-3:] + "th :" \
+                             + " syussya:" + str( schedhandler[0][0] ) \
+                             + " ,taisya:" + str( schedhandler[1][0] ) )
+        elif inputline == "cancel":
+            x = input("input number of item to delete. > ")
+            val = int(x)
+            schedule_obj.cancel( list_schedhandler[val][0][1] )
+            schedule_obj.cancel( list_schedhandler[val][1][1] )
+            del list_schedhandler[val]
+        else:
+            print("invalid input.")
 
-is_debug = False
+is_debug = True
 filepath_schedule = os.path.abspath("./schedule.csv")
 filepath_config   = os.path.abspath("./config.csv")
 filepath_log      = os.path.abspath("./autoworker.log")
+list_schedhandler = []
+#schedule_obj = None
+schedule_obj  = sched.scheduler( time.time, time.sleep )
 
 if __name__ == "__main__":
     if len( sys.argv ) < 2:
